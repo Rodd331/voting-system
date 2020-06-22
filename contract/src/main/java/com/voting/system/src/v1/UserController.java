3cc4cb50a358af1703383d79c17d6269935742c3
rodd <rodd@kali.kali>
2020-06-22 18:51:52 -0300

    Commit 01 criada estrutura de modulos e pacotes, adcionado o Swagger para documentação e feito ajustes iniciais no gradle

---
+++ contract/src/main/java/com/voting/system/src/v1/UserController.java
@@ -0,0 +1,13 @@
+package com.voting.system.src.v1;
+
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "Api Admin Controller", tags = " Api")
+@RequestMapping(path = "/api/v1/admin")
+@RestController
+@AllArgsConstructor
+public class UserController {
+}
---
+++ src/test/java/com/voting/system/src/SrcApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.voting.system.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
