c9f9e8d78da987e31ec1e2e5c5f03b535bb60216
rodd <rodd@kali.kali>
2020-06-23 01:37:57 -0300

    commit 09 Criados os metodos de manipulação dos usuarios nas classes cotrolers de acordo com suas permissões

--- contract/src/main/java/com/voting/system/src/v1/AdminController.java
+++ contract/src/main/java/com/voting/system/src/v1/AdminController.java
@@ -1,13 +1,45 @@
 package com.voting.system.src.v1;
 
+import com.voting.system.src.impl.handler.ExceptionResponse;
+import com.voting.system.src.v1.facade.UserContractFacade;
+import com.voting.system.src.v1.model.response.UserListResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Api(value = "Api Admin Controller", tags = " Api")
 @RequestMapping(path = "/api/v1/admin")
 @RestController
 @AllArgsConstructor
 public class AdminController {
+
+    private UserContractFacade userContractFacade;
+
+    @ApiOperation(value = "Delete user.")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Deleted User"),
+            @ApiResponse(code = 404, message = "User not found", response = ExceptionResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ExceptionResponse.class)
+    })
+    @DeleteMapping("/user/{cpf}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable String cpf) {
+        userContractFacade.deleteUser(cpf);
+    }
+
+    @ApiOperation(value = "List all users.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Successful Operation"),
+            @ApiResponse(code = 404, message = "Not found", response = ExceptionResponse.class),
+            @ApiResponse(code = 500, message = "Internal server error", response = ExceptionResponse.class)
+    })
+    @GetMapping("/user/")
+    public UserListResponse allUsers() {
+        return userContractFacade.allUsers();
+    }
+
 }
--- contract/src/main/java/com/voting/system/src/v1/UserController.java
+++ contract/src/main/java/com/voting/system/src/v1/UserController.java
@@ -1,17 +1,49 @@
 package com.voting.system.src.v1;
 
+import com.voting.system.src.impl.handler.ExceptionResponse;
 import com.voting.system.src.v1.facade.UserContractFacade;
+import com.voting.system.src.v1.model.request.UserRequest;
+import com.voting.system.src.v1.model.response.UserResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Api(value = "Api User Controller", tags = " Api")
-@RequestMapping(path = "/api/v1/user")
+@RequestMapping(path = "/api/v1")
 @RestController
 @AllArgsConstructor
 public class UserController {
 
     private UserContractFacade userContractFacade;
+
+    @ApiOperation(value = "Create new user.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User created"),
+            @ApiResponse(code = 400, message = "Bad Request", response = ExceptionResponse.class),
+            @ApiResponse(code = 500, message = "Internal server error", response = ExceptionResponse.class)
+    })
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@Valid @RequestBody UserRequest user) {
+        return userContractFacade.createUser(user);
+    }
+
+    @ApiOperation(value = "Search for CPF.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "User found"),
+            @ApiResponse(code = 404, message = "User not found", response = ExceptionResponse.class),
+            @ApiResponse(code = 500, message = "Internal server error", response = ExceptionResponse.class)
+    })
+    @GetMapping("/user/{cpf}")
+    public UserResponse findByCPF(@PathVariable String cpf) {
+        return userContractFacade.findByCPF(cpf);
+    }
+
 }
 
